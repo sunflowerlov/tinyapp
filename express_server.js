8a1fc614909ee770a31c4f0d6eb1ec689d6c666e
Lisha Liang <lishaliang1999@gmail.com>
2022-06-16 17:32:58 +0000

    server

---
+++ express_server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/lisha", (req, res) => {
+  const abc = 5 + 5;
+  res.send(`${abc}`);
+  // res.status(200).send(abc)
+})
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
